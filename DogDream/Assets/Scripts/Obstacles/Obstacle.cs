d5d0cfe74aed4694abacfa7fd60a4434da91db7d
Tikhon707 <71888639+Tikhon707@users.noreply.github.com>
2022-11-11 21:14:47 +0500

    Временные файлы

---
+++ DogDream/Assets/Scripts/Obstacles/Obstacle.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    Player player;
+
+    private void Awake()
+    {
+        player = GameObject.Find("Player").GetComponent<Player>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Vector2 pos = transform.position;
+
+        pos.x -= player.rb.velocity.x * Time.fixedDeltaTime;
+        if (pos.x < -100)
+        {
+            Destroy(gameObject);
+        }
+
+        transform.position = pos;
+    }
+}
