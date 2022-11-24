cb4a76d8973f459d3dd83b95b062fbc62ade22e2
Tikhon707 <71888639+Tikhon707@users.noreply.github.com>
2022-11-24 14:47:02 +0500

    Code cleanup
    Удалил мусор

--- DogDream/Assets/Player/Player.cs
+++ DogDream/Assets/Player/Player.cs
@@ -20,7 +20,7 @@ public class Player : MonoBehaviour
     //bool groundHit = false;
 
     public Transform groundCheck;
-    float groundRad = 1f;
+    float groundRad = 0.1f;
     public LayerMask groundLayer;
     public Vector3 movement;
     public Rigidbody2D rb;
@@ -138,13 +138,11 @@ public class Player : MonoBehaviour
             if (!isGrounded)
             {
                 transform.Rotate(0, 0, backwRotation / 1000, Space.Self);
-                //rb.AddTorque(backwRotation * Time.deltaTime, ForceMode2D.Force);
             }
             else
             {
                 isGrounded = false;
                 rb.velocity = Vector2.up * jumpVelocity;
-                //rb.AddForce(transform.up * jumpVelocity * Time.deltaTime * 100f, ForceMode2D.Force);
             }
         }
         else
@@ -152,36 +150,7 @@ public class Player : MonoBehaviour
             if (!isGrounded)
             {
                 transform.Rotate(0, 0, forwRotation / -1000, Space.Self);
-                //rb.AddTorque(forwRotation * -1 * Time.deltaTime, ForceMode2D.Force);
             }
         }*/
     }
-
-    
-    /*
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.tag == "Boost")
-        {
-            rb.AddForce(transform.right * boostSpeed, ForceMode2D.Impulse);
-        }
-        if (collision.tag == "Ground")
-        {
-            groundHit = true;
-            loseState.transform.position = gameObject.transform.position;
-            Instantiate(loseState);
-            Destroy(gameObject);
-            cam.SetActive(false);
-            looseUI.SetActive(true);
-        }
-        if (collision.tag == "Rock")
-        {
-            groundHit = true;
-            loseState.transform.position = gameObject.transform.position;
-            Instantiate(loseState);
-            Destroy(gameObject);
-            cam.SetActive(false);
-            looseUI.SetActive(true);
-        }
-    }*/
 }
